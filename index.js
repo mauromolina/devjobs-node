a9fd98598f27b0ee6fd049785ef515a1e6899897
Mauro <molinamauro2@gmail.com>
2020-09-25 18:45:28 -0300

    set view engine as hbs

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const router = require('./routes');
 
 const app = express();
+
+app.engine('.hbs', 
+    exphbs({
+        defaultLayout: 'layout',
+        extname: '.hbs'
+    })
+);
+
+app.set('view engine', '.hbs');
 
 app.use('/', router());
 
