1c83794e4ee05a4f5c94f1c3b15e5d2174620527
Mauro <molinamauro2@gmail.com>
2020-09-25 22:17:55 -0300

    added mongoose config and dependencies

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
+const db = require('./config/db');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const router = require('./routes');
-const path = require('path')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require('dotenv').config({ path: 'variables.env'});
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
@@ -16,6 +22,18 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection : mongoose.connection
+    })
+}));
+
 app.use('/', router());
 
-app.listen(5000);
+app.listen(process.env.PORT);
